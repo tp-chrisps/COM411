b001001483bb48f4bbe46992ea967479d31c66c5
CHRIS PAHL <0pahlc05@solent.ac.uk>
2025-09-25 14:16:08 +0100

    code to demonstrate use of different data types

--- output_tasks.py
+++ output_tasks.py
@@ -1,6 +1,10 @@
 # display message to the screen
-eye = input("enter a character for the eye")
-print("\n##########")
-print(f"#  {eye}  {eye}  #")
-print("#  ----  #")
-print("##########")
+age = int(input("Enter your age: "))
+height = int(input("Enter your height: "))/100
+weight = int(input("Enter your weight: "))
+bmi = weight / (height * height)
+if bmi < 18.5: print("You are under weight")
+if bmi >= 18.5 and bmi < 25: print("You are heathly weight")
+if bmi >= 25 and bmi < 30: print("you are obese weight")
+if bmi >= 40: print("You are very obese weight")
+print("Your BMI is", bmi)
