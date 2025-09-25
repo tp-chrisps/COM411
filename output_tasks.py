33dd647841afeb8979293f91719b783354a68efb
CHRIS PAHL <0pahlc05@solent.ac.uk>
2025-09-25 13:50:09 +0100

    updated code to display longer multi-line message.

--- output_tasks.py
+++ output_tasks.py
@@ -1,5 +1,5 @@
 # display message to the screen
 print("Welcome to COM411")
-print() # displays blank line
-print("some text")
+print("\nIn week 1 we will learn...")
+print("\n... How to use Git and GitHub")
 
