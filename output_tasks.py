29640c71b16f389abeac4d0e3dfd3de02068f224
CHRIS PAHL <0pahlc05@solent.ac.uk>
2025-09-25 14:41:32 +0100

    using while loops to check conditions on inputs

--- output_tasks.py
+++ output_tasks.py
@@ -1,19 +1,26 @@
 # display message to the screen
-lives = int(input("enter number of lives"))
-energy = int(input("enter number of energy levels"))
-shield = int(input("enter number of shields"))
-print("health has been set\n\n")
+
+lives = input("enter number of lives")
+while not lives.isdigit(): lives = input("enter valid number of lives")
+
+energy = input("enter number of energy levels")
+while not energy.isdigit(): energy = input("enter valid number of energy levels")
+
+shield = input("enter number of shields")
+while not shield.isdigit(): shield = input("enter valid number of shields")
+
+print("health has been set")
 
 heart = ("♥")
 energyIcon=("♦")
 shieldIcon=("✠")
 print("\nLives:", end=(" "))
-for i in range(0, lives):
+for i in range(0, int(lives)):
     print(heart, end="")
 print("\nEnergy:", end=(" "))
-for i in range(0, energy):
+for i in range(0, int(energy)):
     print(energyIcon, end="")
 print("\nShield:", end=(" "))
-for i in range(0, shield):
+for i in range(0, int(shield)):
     print(shieldIcon, end="")
 
