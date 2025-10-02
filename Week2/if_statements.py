bf88df55b7ee5ea04b3f1971c096b2c61a2fa466
CHRIS PAHL <0pahlc05@solent.ac.uk>
2025-10-02 14:30:04 +0100

    added code to demonstrate the use of multiple nested statements

--- Week2/if_statements.py
+++ Week2/if_statements.py
@@ -1,7 +1,21 @@
-cover = input("What type of cover does the book have?")
+phone:int = 0
+while phone ==0:
+    look = input("\nWhere should i look?\nbedroom?\nliving room?\nbathroom?")
+    if look == "bedroom":
+        look = input("where in the bedroom should i look?")
+        if look == "bed": print("Found some shows but no phones")
+        else:print("Found some mess but no phone")
+    elif look == "living room":
+        look = input("where in the living room should i look?\ntable?\nsofa?\ncupboard?")
+        if look == "table":
+            print("Yes! I found me phone!")
+            phone = 1
+        else:print("Found some stuff but no phone")
+    elif look == "bathroom":
+        look = input("where in the bathroom should i look?\nbath?\ncabinet?")
+        if look == "bath":print("Found a rubber duck but no phone")
+        else: print("Found some stuff but no phone")
+    else: print("I dont know where that is but I will keep looking.")
 
-if cover =="soft":
-    bound = input("Is the book perfect bound?")
-    if bound == "yes":print("Soft cover, perfect bound books are very popular!")
-    else:print("Soft covers with coils or stitches are great for short books")
-if cover =="hard":print("Books with hard covers can be more expensive!")
+
+
