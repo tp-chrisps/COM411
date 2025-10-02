d86a73c04b781c0b906a9c4f51f6db85eb67f071
CHRIS PAHL <0pahlc05@solent.ac.uk>
2025-10-02 14:43:45 +0100

    added code to demonstrate the use of a logical operator

--- Week2/if_statements.py
+++ Week2/if_statements.py
@@ -1,21 +1,6 @@
-phone:int = 0
-while phone ==0:
-    look = input("\nWhere should i look?\nbedroom?\nliving room?\nbathroom?")
-    if look == "bedroom":
-        look = input("where in the bedroom should i look?")
-        if look == "bed": print("Found some shows but no phones")
-        else:print("Found some mess but no phone")
-    elif look == "living room":
-        look = input("where in the living room should i look?\ntable?\nsofa?\ncupboard?")
-        if look == "table":
-            print("Yes! I found me phone!")
-            phone = 1
-        else:print("Found some stuff but no phone")
-    elif look == "bathroom":
-        look = input("where in the bathroom should i look?\nbath?\ncabinet?")
-        if look == "bath":print("Found a rubber duck but no phone")
-        else: print("Found some stuff but no phone")
-    else: print("I dont know where that is but I will keep looking.")
-
+type = input("What type of adventure should I take?")
+if type == "short" or type =="scary":print("Entering the dark forest!")
+elif type == "safe" or type == "long":print("Taking the safe route!")
+else: print("Not sure which route to take")
 
 
