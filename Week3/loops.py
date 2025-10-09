4600a28bfc245dc3362128ddb4b7220b661a7d25
CHRIS PAHL <0pahlc05@solent.ac.uk>
2025-10-09 14:42:06 +0100

    loops in string indexing

--- Week3/loops.py
+++ Week3/loops.py
@@ -1,9 +1,4 @@
-level = int(input("What level of brightness is required? "))
-print("Adjusting brightness")
-a = "**"
-b = "**"
-for i in range(0,level,2):
-    print("Brightness level: ",end="")
-    print(a)
-    a = a + b
-print("Complete!")
+word = input("What word do you see?\n")
+print("Displaying index positions")
+for index in range(len(word)):
+    print(f"index {index}, {word[index]}")
