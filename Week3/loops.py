c23eeecea89c2a01f9f516873f892dac33788b71
CHRIS PAHL <0pahlc05@solent.ac.uk>
2025-10-09 13:36:44 +0100

    while loop with len function

--- Week3/loops.py
+++ Week3/loops.py
@@ -1,7 +1,5 @@
-bars = int(input("How many bars should be charged?"))
-bar = "█"
-ba = "█"
-for i in range(bars):
-    print(f"Charging: {bar}")
-    bar = f"{bar}{ba}"
-print("The battery is fully charged")
+phrase = input("Please enter a phrase: ")
+i = 0
+while i < len(phrase):
+    print("Hi", end=" ")
+    i += 1
