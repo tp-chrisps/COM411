505483784365f378ae03893a59681bcfc4eb2fc3
CHRIS PAHL <0pahlc05@solent.ac.uk>
2025-10-09 13:22:44 +0100

    While loop with a counter

--- Week3/loops.py
+++ Week3/loops.py
@@ -1,2 +1,8 @@
-i = input("How many apples should I remove?")
-for j in range(int(i)):print("Removed apple.")
+count = input("How many obstacles must I avoid?")
+num = 0
+while int(count) > 0:
+    print("Avoiding...",end="")
+    num = num + 1
+    print(f"Done! {num} obstacles avoided!")
+    count = int(count) - 1
+print("All obstacles have been avoided")
