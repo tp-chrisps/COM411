9b51b849e8581bc7238403b4d5029c1f153d7488
CHRIS PAHL <0pahlc05@solent.ac.uk>
2025-10-16 14:05:31 +0100

    user defined function with a nested decision

--- Week5/custom_functions.py
+++ Week5/custom_functions.py
@@ -1,4 +1,7 @@
-def listen():
-    sound = input("What sound did you hear?")
-    print(f"That was a loud {sound}!")
-listen()
+def identify():
+    see = input("What do you see? ")
+    if see == "a large boulder":
+        print("It's time to run!")
+    else:
+        print("We will be fine.")
+identify()
