85999369609e874c8cbb3187b4f283dc7175da46
CHRIS PAHL <0pahlc05@solent.ac.uk>
2025-10-23 14:37:11 +0100

    returning a tuple from a function

--- Week5/tuple_tasks.py
+++ Week5/tuple_tasks.py
@@ -6,4 +6,9 @@ def run_task1():
     tuple = likelihood()
     print(f"Minimun likelihood of falling: {min(tuple)}% ")
 
+def run_task2():
+    tuple = likelihood()
+    return max(tuple)
+
 run_task1()
+print(f"Maximum likelihood of falling: {run_task2()}%")
