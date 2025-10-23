3d80da1b8349e7a6c6cc3b693906cbc42c4be84e
CHRIS PAHL <0pahlc05@solent.ac.uk>
2025-10-23 14:42:09 +0100

    nested tuples in a list

--- Week5/tuple_tasks.py
+++ Week5/tuple_tasks.py
@@ -2,6 +2,10 @@ def likelihood():
     likelihoods = (50, 38, 27, 99, 4)
     return likelihoods
 
+def steps():
+    likelihoods = [("Step 1", 50), ("Step 2", 38), ("Step 3", 27), ("Step 4", 99), ("Step 5", 4)]
+    return likelihoods
+
 def run_task1():
     tuple = likelihood()
     print(f"Minimun likelihood of falling: {min(tuple)}% ")
@@ -10,5 +14,18 @@ def run_task2():
     tuple = likelihood()
     return max(tuple)
 
+def run_task3():
+    list = steps()
+    good_steps = []
+    bad_steps = []
+    for step in list:
+        if step[1] < 50:
+            good_steps.append(step)
+        else:
+            bad_steps.append(step)
+    print(f"Good steps: {len(good_steps)}, Bad steps: {len(bad_steps)}")
+
+
 run_task1()
 print(f"Maximum likelihood of falling: {run_task2()}%")
+run_task3()
