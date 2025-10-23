bc7077d01b629a07f8072db496d99ca53f2ee569
CHRIS PAHL <0pahlc05@solent.ac.uk>
2025-10-23 13:36:45 +0100

    Code to demonstrate use of a list

---
+++ Week5/list_tasks.py
@@ -0,0 +1,13 @@
+def directions():
+    steps = []
+    steps.append("Move Forward")
+    steps.append("Move Left")
+    steps.append("Move Right")
+    steps.append("Move Downwards")
+    return steps
+
+def run_task1():
+    if __name__ == "__main__":
+        print(directions())
+
+run_task1()
