17c7a37023b64e09f6b11961bb99f3374b73cb33
tp-chrisps <0pahlc05@solent.ac.uk>
2025-11-02 00:23:49 +0000

    reading txt file

--- Week6/txt_file_tasks.py
+++ Week6/txt_file_tasks.py
@@ -4,7 +4,32 @@ def cwd():
     print(f"The current working directory is: {filepath}")
     print(f"The directory contains the following files:{os.listdir(filepath)}")
 
+def display_chars(path, num):
+    with open("library.txt") as file:
+        data = file.read(num)
+    print(data)
+    file.close()
+
+def display_line(path):
+    with open("library.txt") as file:
+        data = file.readline()
+    print(data)
+    file.close()
+
+def display_text(path):
+    with open("library.txt") as file:
+        data = file.read()
+    print(data)
+    file.close()
+
 def run():
     cwd()
 
-run()
+def run_task2():
+    path = "library.txt"
+    display_chars(path, 5)
+    display_line(path)
+    display_text(path)
+
+if __name__ == "__main__":
+    run_task2()
