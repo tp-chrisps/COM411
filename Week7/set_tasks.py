ef23f3f43acd8cfde0bff4f4d41dd092cab9b9fd
CHRIS PAHL <0pahlc05@solent.ac.uk>
2025-11-06 13:21:00 +0000

    how to create a set from a list

--- Week7/set_tasks.py
+++ Week7/set_tasks.py
@@ -7,7 +7,19 @@ def observed():
                     "House"}
     return observations
 
-def run_task1():
-    print(observed())
+def observed_items():
+    observations = {}
+    for i in range(7):
+        observ = input("Please enter an observation:\n")
+        if observ not in observations:
+            observations[observ] = 1
+        else:
+            observations[observ] += 1
+    for item in observations:
+        print(f"{item}: observed {observations[item]}times ")
+def run_task2():
+    observed_items()
 
-run_task1()
+if __name__ == "__main__":
+    run_task2()
+
