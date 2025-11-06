b954146aafb83489d84c62ac49a5426b1146c5e4
CHRIS PAHL <0pahlc05@solent.ac.uk>
2025-11-06 13:13:53 +0000

    use of a simple set

---
+++ Week7/set_tasks.py
@@ -0,0 +1,13 @@
+def observed():
+    observations = {"Car",
+                    "Sky Scraper",
+                    "Sky Scraper",
+                    "Bike",
+                    "House",
+                    "House"}
+    return observations
+
+def run_task1():
+    print(observed())
+
+run_task1()
