5da369b8799c1bf7bb181c4ed32e7fb1e341822e
tp-chrisps <0pahlc05@solent.ac.uk>
2025-11-16 15:33:58 +0000

    line plot

---
+++ Week8/plots.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+def display_line(x, y):
+    plt.plot(x, y)
+    plt.show()
+
+def run_task1():
+    x_values = [1,2,3,4,5]
+    y_values = [1,4,9,16,25]
+    display_line(x_values, y_values)
+
+if __name__ == "__main__":
+    run_task1()
