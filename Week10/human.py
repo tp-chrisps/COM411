5b9d6a156c31ece5d65b95cca1938b15494961f1
CHRIS PAHL <0pahlc05@solent.ac.uk>
2025-11-20 13:29:50 +0000

    basic human and robot classes

---
+++ Week10/human.py
@@ -0,0 +1,9 @@
+class Human:
+    MAX_ENERGY = 100
+    def __init__(self, name, age, energy):
+        self.name = name
+        self.age = age
+        self.energy = energy
+
+    def display(self):
+        print(f"I am {self.name}")
---
+++ Week10/main.py
@@ -0,0 +1,7 @@
+from human import Human
+from robot import Robot
+if __name__ == '__main__':
+    human = Human("Human", 0, 0)
+    human.display()
+    robot = Robot("Robot", 0, 0)
+    robot.display()
